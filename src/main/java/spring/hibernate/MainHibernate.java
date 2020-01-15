f177336f5863c6a3714e8b5c52c66d208c2922a7
Marzena Gielmuda <m.gielmuda@wp.pl>
2020-01-15 08:43:54 +0100

    15-01-2019 try connect metods with hibernate

--- src/main/java/spring/hibernate/MainHibernate.java
+++ src/main/java/spring/hibernate/MainHibernate.java
@@ -2,11 +2,14 @@ package spring.hibernate;
 
 import java.util.Date;
 import java.util.List;
+import spring.hibernate.EmployeeDao;
+import spring.hibernate.Employees;
+
 
 public class MainHibernate {
     public static void main(String[] args) {
         EmployeeDao employeeDao = new EmployeeDao();
-        Employees employee = new Employees("Test", "Test", "Test", "Test", 1000, 18 , new Date(), 1);
+        Employees employee = new Employees(2,"Test", "Test", "Test", "Test", 1000, 18 , new Date(), 1, "test@test");
         employeeDao.saveEmployee(employee);
         List<Employees> employeesList = employeeDao.getEmployees();
 
